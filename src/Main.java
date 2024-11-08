ae796b429019909fb29abe01e50cfece8a7200cb
hoanh <hoanhphap582002@gmail.com>
2024-11-08 21:11:09 +0700

    PacMan

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac Man");
+
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacMan = new PacMan();
+        frame.add(pacMan);
+        frame.pack();
+        pacMan.requestFocus();
+        frame.setVisible(true);
+    }
+}
